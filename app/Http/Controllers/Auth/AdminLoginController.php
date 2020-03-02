2f2562528c4b2152b27b00c1feb6231e02062605
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-03-02 10:16:10 -0300

    :construction: Cria regra de login admin

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminLoginController extends Controller
 {
@@ -12,7 +13,22 @@ class AdminLoginController extends Controller
         $this->middleware('guest:admin');
     }
     public function login(Request $request) {
-        return 'ok';
+        $this->validate($request, [
+            $this->username() => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = [
+            'user' => $request->email,
+            'password' => $request->password
+        ];
+
+        $authOk = Auth::guard('admin')->attempt($credentials, $request->remember);
+
+        if ($authOk) {
+            return redirect()->intended(route('admin.dashboard'));
+        }
+        return redirect()->back()->withInput($request->only('email', 'remember'));
     }
 
     public function index() {
